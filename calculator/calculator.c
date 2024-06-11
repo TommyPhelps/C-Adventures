54f02fb5132d918d441e1f975cca8332aa84fbd0
Tommy Phelps <tommydustinphelps@gmail.com>
2024-06-11 12:37:56 -0500

    Add addition function

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+int add(int num1, int num2) {
+    int result = 0;
+
+    if (num1 != 0 && num2 != 0) {
+        result = num1 + num2;
+    }
+    
+    return result;
+}
+
 int main() {
     int firstNum;
     int secondNum;
+    int result;
+
     scanf("%d %d", &firstNum, &secondNum);
-    printf("Your first number is %d. Your second number is %d.", firstNum, secondNum);
+    result = add(firstNum, secondNum);
+    printf("%d + %d = %d", firstNum, secondNum, result);
+
+    return 0;
 }
