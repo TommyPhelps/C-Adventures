f8c6ecdab6f454a5f240e5c010c5ed52147e9793
Tommy Phelps <tommydustinphelps@gmail.com>
2024-06-11 12:31:07 -0500

    Add code to get user input

---
+++ calculator/calculator.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int firstNum;
+    int secondNum;
+    scanf("%d %d", &firstNum, &secondNum);
+    printf("Your first number is %d. Your second number is %d.", firstNum, secondNum);
+}
