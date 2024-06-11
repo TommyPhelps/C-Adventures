a5621bab5f445262d16897b8c90fb4a79bdf8bcc
Tommy Phelps <tommydustinphelps@gmail.com>
2024-06-11 12:50:16 -0500

    Add subtraction function and user input for the operation

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -10,20 +10,40 @@ int add(int num1, int num2) {
     return result;
 }
 
+int sub(int num1, int num2) {
+    int result = 0;
+
+    if (num1 != 0 && num2 != 0) {
+        result = num1 - num2;
+    }
+    
+    return result;
+}
+
 int main() {
     int firstNum;
     int secondNum;
     int result;
+    char operation;
 
     printf("Enter the numbers that you would like to calculate!\n\n");
 
     printf("Number 1: ");
-    scanf("%d", &firstNum);
+    scanf(" %d", &firstNum);
 
     printf("Number 2: ");
-    scanf("%d", &secondNum);
+    scanf(" %d", &secondNum);
 
-    result = add(firstNum, secondNum);
+    printf("Enter the operation you would like to perform! +, -\n\n");
+
+    printf("Operation: ");
+    scanf(" %c", &operation);
+
+    if (operation == '+') {
+        result = add(firstNum, secondNum);
+    } else if (operation == '-') {
+        result = sub(firstNum, secondNum);
+    }
 
     printf("\n%d + %d = %d", firstNum, secondNum, result);
 
