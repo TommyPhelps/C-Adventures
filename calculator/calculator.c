b553bec3f5cc95899e92be370e6f19d321eaef78
Tommy Phelps <tommydustinphelps@gmail.com>
2024-06-11 21:50:46 -0500

    Add subtraction function + conditional to subtract

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -20,6 +20,16 @@ int sub(int num1, int num2) {
     return result;
 }
 
+int mul(int num1, int num2) {
+    int result = 0;
+
+    if (num1 != 0 && num2 != 0) {
+        result = num1 * num2;
+    }
+    
+    return result;
+}
+
 int main() {
     int firstNum;
     int secondNum;
@@ -34,7 +44,7 @@ int main() {
     printf("Number 2: ");
     scanf(" %d", &secondNum);
 
-    printf("Enter the operation you would like to perform! +, -\n\n");
+    printf("Enter the operation you would like to perform! +, -, *\n\n");
 
     printf("Operation: ");
     scanf(" %c", &operation);
@@ -43,7 +53,9 @@ int main() {
         result = add(firstNum, secondNum);
     } else if (operation == '-') {
         result = sub(firstNum, secondNum);
-    }
+    } else if (operation == '*') {
+		result = mul(firstNum, secondNum);
+	}
 
     printf("\n%d + %d = %d", firstNum, secondNum, result);
 
